e1544c945315e60438bf71cf4909f764141e579e
Geethasrivalli <geethasrivallimolleti@gmail.com>
2025-04-04 15:48:34 +0530

    day 2 task

--- input.js
+++ input.js
@@ -3,4 +3,16 @@ function submit() {
     console.log(input[0].value);
     let name = document.getElementById("name");
     console.log(name.value);
+    let radio = document.getElementById("radio");
+    console.log(radio.value);
+    let checkbox = document.getElementById("checkbox");
+    console.log(checkbox.value);
+    let date = document.getElementById("date");
+    console.log(date.value);
+    let email = document.getElementById("email");
+    console.log(email.value);
+    let file = document.getElementById("file");
+    console.log(file.value);
+    let datetime = document.getElementById("datetime");
+    console.log(datetime.value);
 }
